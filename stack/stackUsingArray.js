a2c4e6a64af6adc49aea23f80990f5d597e43cf8
Andela-eugene <eugene.liyai@andela.com>
2018-12-06 17:12:32 +0300

    [Feat: #] implement stack with array

---
+++ stack/stackUsingArray.js
@@ -0,0 +1,28 @@
+
+export default class {
+  constructor() {
+    this._items = []
+  }
+
+  push(item) {
+    this._items.unshift(item);
+  }
+
+  pop() {
+    if(this._items.length === 0) {
+      throw new Error('Stack is empty')
+    }
+    return this._items.shift();
+  }
+
+  peek() {
+    if(this._items.length === 0) {
+      throw new Error('Stack is empty')
+    }
+    return this._items[0];
+  }
+
+  count() {
+    return this._items.length
+  }
+}
