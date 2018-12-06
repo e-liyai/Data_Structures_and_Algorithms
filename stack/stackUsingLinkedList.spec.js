e4e38d22f0f2228004a0431779226f11386dd775
Andela-eugene <eugene.liyai@andela.com>
2018-12-06 15:27:27 +0300

    [Feat: #] add stack tests

---
+++ stack/stackUsingLinkedList.spec.js
@@ -0,0 +1,40 @@
+import Stack from './stackUsingLinkedList'
+
+let stack
+describe('test stack list data structure', () => {
+  beforeEach(() => {
+    stack = new Stack()
+  })
+
+  test('test if stack is empty', () => {
+    expect(stack.count()).toBe(0)
+  })
+
+  test('test push item to stack', () => {
+    stack.push(5)
+    expect(stack.count()).toBe(1)
+    stack.push(34)
+    expect(stack.count()).toBe(2)
+  })
+
+  test('test peek in stack', () => {
+    stack.push(5)
+    stack.push(4)
+    stack.push(3)
+    expect(stack.peek()).toBe(3)
+  })
+
+  test('test pop in stack', () => {
+    stack.push(5)
+    stack.push(4)
+    stack.push(3)
+    expect(stack.pop()).toBe(3)
+    expect(stack.pop()).toBe(4)
+    expect(stack.pop()).toBe(5)
+  })
+
+  test('test pop and peek error handling in stack', () => {
+    expect(() => stack.pop()).toThrowError('Stack is empty')
+    expect(() => stack.peek()).toThrowError('Stack is empty')
+  })
+})
