23c691999de183479ac8ce6ada2b09c98cab277f
Andela-eugene <eugene.liyai@andela.com>
2018-12-06 20:53:31 +0300

    [Feat: #] implement queue and dequeue tests

--- queue/queueUsingLinkedList.spec.js
+++ queue/queueUsingLinkedList.spec.js
@@ -1,3 +1,30 @@
 import Queue from './queueUsingLinkedList'
 
 let queue
+describe('test stack list data structure', () => {
+  beforeEach(() => {
+    queue = new Queue()
+  })
+
+  test('test if queue is empty', () => {
+    expect(queue.count()).toBe(0)
+  })
+
+  test('test enqueue item to queue', () => {
+    queue.enqueue(32)
+    expect(queue.peek()).toBe(32)
+  })
+
+  test('test enqueue and dequeue items to queue', () => {
+    queue.enqueue(32)
+    queue.enqueue(53)
+    queue.enqueue(67)
+    queue.enqueue(11)
+    expect(queue.peek()).toBe(32)
+    expect(queue.dequeue()).toBe(32)
+    expect(queue.dequeue()).toBe(53)
+    expect(queue.dequeue()).toBe(67)
+    expect(queue.dequeue()).toBe(11)
+    expect(queue.count()).toBe(0)
+  })
+})
