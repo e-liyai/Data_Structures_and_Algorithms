b6c2554b9442978d9752d9f215e3f20a626ae3d2
Andela-eugene <eugene.liyai@andela.com>
2018-12-06 19:56:42 +0300

    [Feat: #] implement postfix

---
+++ examples/stack/postfixCalculator.js
@@ -0,0 +1,51 @@
+import Stack from '../../stack/stackUsingArray'
+import readline from 'readline'
+
+const readArguments = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let stack = new Stack()
+
+const postFixCalculator = () => {
+  // example input 567*+1-
+  readArguments.question('Add value and operator: ', (values) => {
+
+    console.log('\nCalculating...\n')
+
+    for (let item = 0; item < values.length; item++){
+      let char = values.charAt(item)
+      if (!isNaN(char)) {
+        stack.push(char)
+      } else {
+        let rightHandValue = stack.pop()
+        let leftHandValue = stack.pop()
+        switch (char) {
+          case '+':
+            stack.push(parseInt(leftHandValue)+parseInt(rightHandValue))
+            break
+          case '-':
+            stack.push(leftHandValue-rightHandValue)
+            break
+          case '*':
+            stack.push(leftHandValue*rightHandValue)
+            break
+          case '/':
+            stack.push(leftHandValue/rightHandValue)
+            break
+          case '%':
+            stack.push(leftHandValue%rightHandValue)
+            break
+          default:
+            throw new Error(`Unrecognized token ${item}`)
+        }
+      }
+    }
+    console.log('Answer is: ', stack.pop())
+    readArguments.close();
+  });
+
+}
+
+postFixCalculator()
