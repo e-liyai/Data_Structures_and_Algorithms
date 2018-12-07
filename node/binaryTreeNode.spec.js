9bcfc83092b1bbdf6cc730f47cb0882d5d3d2fc6
Andela-eugene <eugene.liyai@andela.com>
2018-12-07 12:48:06 +0300

    [Feat: #] add binary tree node tests

---
+++ node/binaryTreeNode.spec.js
@@ -0,0 +1,22 @@
+import BinaryNode from './binaryTreeNode'
+
+let node1
+let node2
+let node3
+describe('test data structure node', () => {
+  beforeEach(() => {
+    node1 = new BinaryNode(4)
+    node2 = new BinaryNode(6)
+    node3 = new BinaryNode(2)
+  })
+
+  test('successfuly create node object', () => {
+    expect(node1._value).toBe(4)
+  })
+  test('successfuly add next and previous pointers', () => {
+    node1.nodeLeft = node3
+    node1.nodeRight = node2
+    expect(node1._left._value).toBe(2)
+    expect(node1._right._value).toBe(6)
+  })
+})
