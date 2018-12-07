c70d8a8c81e603ff42d34fb08a2340b77e8cfb0d
Andela-eugene <eugene.liyai@andela.com>
2018-12-07 12:36:20 +0300

    [Feat: #] add binary tree node

---
+++ node/binaryTreeNode.js
@@ -0,0 +1,19 @@
+export default class {
+  constructor(value, left=null, right=null) {
+    this._value = value
+    this._left = left
+    this._right = right
+  }
+
+  set nodeValue(value) {
+    this._value = value
+  }
+
+  set nodeLeft(left) {
+    this._left = left
+  }
+
+  set nodeRight(right) {
+    this._right = right
+  }
+}
