315785fcd996eb6a3636f340c050e2b23573425c
Andela-eugene <eugene.liyai@andela.com>
2018-12-07 18:52:53 +0300

    [Feat: #] implement add and remove binary tree nodes

--- trees/binaryTree.js
+++ trees/binaryTree.js
@@ -38,19 +38,78 @@ export default class {
   findWithParent(value) {
     let current = this._head
     let parent = null
+    let result = null
 
     while(current._value !== null) {
       let compareResult = current.compareWith(value)
       if (compareResult) {
         parent = current
-        current = current._left
+        current = current._right
       } else if (compareResult === -1) {
         parent = current
-        current = current._right
+        current = current._left
       } else {
+        result = {currentNode: current, parentNode: parent}
         break
       }
     }
-    return {currentNode: current, parentNode: parent}
+    return result
+  }
+
+  remove(value) {
+    let current
+    let parent
+
+    current = findWithParent(value)
+    if(current === null) return false
+    this._count--
+
+    parent = current.parentNode
+    current = current.currentNode
+
+    // case 1: current has no right child, then current's left replaces current
+    if(current._right === null) {
+      if(parent === null) this._head = current._left
+      else {
+        let result = parent.compareWith(current._value)
+        if (result) parent.nodeLeft = current._left
+        else if (result < 0) parent.nodeRight = current._left
+      }
+    }
+    // case 2: if current's right child has no left child, then current's right child replaces current
+    else if(current._right._left === null) {
+      current._right.nodeLeft = current._left
+
+      if(parent === null) this._head = current._right
+      else {
+        let result = parent.compareWith(current._value)
+        if (result) parent.nodeLeft = current._right
+        else if (result < 0) parent.nodeRight = current._right
+      }
+    }
+    // case 3: if current's right child has a left child, then replace current with
+    //  current's right child's left most node
+    else {
+      let leftMostNode = current._right._left
+      let leftMostParent = current._right
+
+      while(leftMostNode._left === null) {
+        leftMostParent = leftMostNode
+        leftMostNode = leftMostNode._left
+      }
+
+      leftMostParent.nodeLeft = leftMostNode._right
+
+      leftMostNode.nodeLeft = current._left
+      leftMostNode.nodeRight = current._right
+
+      if (parent === null) this._head = leftMostNode
+      else {
+        let result = parent.compareWith(current._value)
+        if(result) parent.nodeLeft = leftMostNode
+        if(result < 0) parent.nodeRight = leftMostNode
+      }
+    }
+    return true
   }
 }
