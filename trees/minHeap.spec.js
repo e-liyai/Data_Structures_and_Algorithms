a6cf20c906f19e3fe81f8c5dea712ad5f9af36e5
Andela-eugene <eugene.liyai@andela.com>
2018-12-16 23:43:12 +0300

    [Feat: #] implement add test

--- trees/minHeap.spec.js
+++ trees/minHeap.spec.js
@@ -11,4 +11,12 @@ describe('test stack list data structure', () => {
     minHeap.add(6)
     expect(minHeap.size()).toBe(1)
   })
+
+  test('add items to min heap', () => {
+    minHeap.add(6)
+    minHeap.add(3)
+    minHeap.add(9)
+    expect(minHeap.size()).toBe(3)
+    expect(minHeap.peek()).toBe(3)
+  })
 })
