8f5e714906afc465353e11d40b739d33ca642f1d
Andela-eugene <eugene.liyai@andela.com>
2018-12-16 22:51:45 +0300

    [Feat: #] add swap in min heap

--- trees/minHeap.js
+++ trees/minHeap.js
@@ -10,4 +10,16 @@ export default class {
     hasLeftChild(index) { return getLeftChildIndex(index) < this.heap.length}
     hasRightChild(index) { return getRightChildIndex(index) < this.heap.length}
     hasParent(index) { return getParentIndex(index) >= 0}
+
+    leftChild(index) { return this.heap[getLeftChildIndex(index)]}
+    rightChild(index) { return this.heap[getRightChildIndex(index)]}
+    parent(index) { return this.heap[getParentIndex(index)]}
+
+    swap(indexOne, indexTwo) {
+      const temp = this.heap[indexOne]
+      this.heap[indexOne] = this.heap[indexTwo]
+      this.heap[indexTwo] = temp
+    }
+
+
 }
