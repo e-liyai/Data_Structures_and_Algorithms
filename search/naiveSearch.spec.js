5187873c632bb825b0b2f9994f670fa23d29f953
Andela-eugene <eugene.liyai@andela.com>
2019-02-12 15:41:03 +0300

    [feat: #] add other test cases

--- search/naiveSearch.spec.js
+++ search/naiveSearch.spec.js
@@ -2,6 +2,14 @@ import search from './naiveSearch'
 
 describe('test naive search', () => {
 	test('success test', () => {
-		expect(search('man', 'woman')).toBe(true)
+		expect(search('man', 'woman')).toBeTruthy
+	})
+
+	test('fail check test', () => {
+		expect(search('father', 'woman')).toBeFalsy
+	})
+
+	test('empty variable test', () => {
+		expect(search('', 'empty')).toBeFalsy
 	})
 })
