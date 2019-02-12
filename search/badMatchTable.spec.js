cbd7509f4a9afbc9a6b3f265d0a9899fd2b98499
Andela-eugene <eugene.liyai@andela.com>
2019-02-12 18:07:29 +0300

    [feat: #] add dict test

--- search/badMatchTable.spec.js
+++ search/badMatchTable.spec.js
@@ -10,4 +10,10 @@ describe('test bad match table', () => {
   	expect(badMatchTable._defaultValue).toBe(5)
   	expect(badMatchTable._pattern).toBe('TRUTH')
   })
+
+  test('test table generated', () => {
+  	expect(badMatchTable._distances['T']).toBe(1)
+  	expect(badMatchTable._distances['R']).toBe(3)
+  	expect(badMatchTable._distances['U']).toBe(2)
+  })
 })
