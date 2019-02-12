06a395f64964c6d2f5927150fc7444b0aff93e8b
Andela-eugene <eugene.liyai@andela.com>
2019-02-12 18:03:04 +0300

    [feat: #] add for loop in constractor

--- search/badMatchTable.js
+++ search/badMatchTable.js
@@ -3,9 +3,9 @@ export default class {
 		this._defaultValue = pattern.length
 		this._distances = {}
 		this._pattern = pattern
-	}
 
-	for (let i = 0; i < this._defaultValue -1, i++) {
-		this._distances[this._pattern[i]] = this._pattern.length -1 -i;
+		for (let i = 0; i < this._defaultValue -1; i++) {
+			this._distances[this._pattern[i]] = this._pattern.length -1 -i;
+		}
 	}
 }
