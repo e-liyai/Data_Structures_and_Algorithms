2da85bd1c7540a48404eeb00a30dcbd3fc2f4cb7
Andela-eugene <eugene.liyai@andela.com>
2019-02-12 15:24:57 +0300

    [feat: #] add naive search

---
+++ search/naïveSearch.js
@@ -0,0 +1,14 @@
+const search = (toFind, toSearch) => {
+	if(toFind.length > 0 && toSearch.length > 0) {
+		for(let startIndex = 0; startIndex < toSearch.length - toFind.length; startIndex++) {
+			let matchCount = 0
+			while (toSearch[startIndex + matchCount] === toFind[matchCount]) {
+				matchCount++
+				if(toFind.length === matchCount) return true
+			}
+		}
+	}
+	return false
+}
+
+module.exports = search
