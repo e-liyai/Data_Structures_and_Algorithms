0b32138c323235989906cb3f820aefea1dbda479
Andela-eugene <eugene.liyai@andela.com>
2019-07-12 18:53:25 +0300

    feat: add incorrect hash

--- hashingAlgorithm/simpleAdditionHash.spec.js
+++ hashingAlgorithm/simpleAdditionHash.spec.js
@@ -7,4 +7,7 @@ describe('test simple hashing algorithm', () => {
   test('test number input', () => {
     expect(hash(4293)).toEqual(18)
   })
+  test('test incorrect hash', () => {
+    expect(hash(4293)).not.toEqual(20)
+  })
 })
